1addf2af2b04ad42665392a31897070517714b26
Dina Berry <dinaberry@outlook.com>
2023-03-14 22:27:16 -0700

    azure-samples and staticwebdev return full sets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs/yargs'
 import { reposCursorMgr } from './query'
-import JSON5 from 'json5'
+import fs from 'fs/promises'
+import path from 'path'
 
 const argv = yargs(hideBin(process.argv)).argv
 const noop = (_: string) => {}
@@ -12,24 +13,24 @@ Usage:
     node index.ts --org <orgName> --pat <pat> [options]
 
     Required:
-    --org <orgName> - The name of the organization to query
-    --pat <pat> - The personal access token to use for authentication
+    --org <orgName> - The name of the organization to query. Ex: 'staticwebdev' or 'azure-samples'.
+    --pat <pat>     - The personal access token to use for authentication. Or process.env.PAT.
 
     Optional:
     --verbose <true|false> - Whether to print verbose output. Defaults to false.
-    --max <number> - The maximum number of items to return. Defaults to 1. -1 means all.
-    --page <number> - The number of items to return per page. Defaults to 10.
-    --delay <number> - The number of milliseconds to delay between requests. Defaults to 900.
-    --top <number> - The number of items to return, sorted by weight. Defaults to 5. -1 means return all.
-    --prop <string> - The property to return. Defaults to 'repositoryName'. 'all' means return all properties.
+    --max <number>         - The maximum number of items to return, sorted by weight. Defaults to 1. -1 means all.
+    --page <number>        - The number of items to return per page. Defaults to 10.
+    --delay <number>       - The number of milliseconds to delay between requests. Defaults to 900.
+    --prop <string>        - The property to return. Defaults to 'repositoryName'. 'all' means return all properties.
+    --file <string>        - The file to write the output to. 
 
 `
 
 async function main() {
-  if (argv['org']) {
+  if (argv['org'] && (argv['pat'] || process.env.PAT)) {
     // required
     const orgName = argv['org']
-    const pat = argv['pat']
+    const pat = argv['pat'] || process.env.PAT
 
     // optional
     const verbose = argv['verbose'] || false
@@ -64,17 +65,26 @@ async function main() {
         log
       )
 
-      const top = argv['top'] || 5
-      if (top !== -1) {
-        result = result.slice(0, top)
+      if (maxItems !== -1) {
+        result = result.slice(0, maxItems)
+        log(`Returning ${result.length} items`)
       }
 
       const prop = argv['prop'] || 'repositoryName'
       if (prop !== 'all') {
         result = result.map((x) => x.repositoryName)
+        log(`Returning '${prop}' property values`)
       }
 
-      console.log(JSON.stringify(result))
+      const file = argv['file']
+      if (file) {
+        const fileWithPath = path.join(__dirname, `../${file}`)
+        await fs.writeFile(fileWithPath, JSON.stringify(result, null, 2))
+        log(`Wrote output to ${fileWithPath}`)
+      } else {
+        log(`Output to console`)
+        console.log(JSON.stringify(result))
+      }
     }
   } else {
     console.log(instructions)
