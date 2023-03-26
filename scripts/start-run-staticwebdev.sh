fe157e46040f6ac05a8baab8262e9a416929fdf5
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-03-26 14:03:10 -0700

    update to live run uses env pat not file pat

--- scripts/start-run-staticwebdev.sh
+++ scripts/start-run-staticwebdev.sh
@@ -1,2 +1,2 @@
-export $(cat .env | grep -v ^# | xargs) >/dev/null
+#export $(cat .env | grep -v ^# | xargs) >/dev/null
 node ./package/index.js --org staticwebdev --page 100 --verbose true --delay 2000 --max -1 --prop repositoryName --file scope/staticwebdev.json --pat $PAT --top 5 --sort weight --sortdir desc
